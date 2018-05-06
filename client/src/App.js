013105e41af9b01ed374cc51f44f239635b540f4
KaroonK <khatiwada.karoon@gmail.com>
2018-05-06 00:07:03 -0400

    Second Push- NavBar Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, withRouter } from 'react-router-dom';
 import Home from './Home';
 import Test from './Test';
 import Loans from './Loans';
@@ -9,40 +9,113 @@ import Clients from './Clients';
 import Collections from './Collections';
 import Employees from './Employees';
 import Investments from './Investments';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import './styles/icons.css';
+import homeImg from './images/home.png';
+import clientsImg from './images/clients.png';
+import accImg from './images/accounts.png';
+import empImg from './images/employee.png';
+import branchImg from './images/branch.png';
+import loansImg from './images/loans.png';
+import insImg from './images/investment.png';
+import colImg from './images/collect.png';
+var whiteColor = { "White": "#ffffff" };
 
 class App extends Component {
+
   render() {
 
     return (
       <div>
-        <nav className="navbar navbar-light">
-          <ul className="nav navbar-nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/clients">Clients</Link></li>
-            <li><Link to="/accounts">Accounts</Link></li>
-            <li><Link to="/employees">Employees</Link></li>
-            <li><Link to="/branch">Branch</Link></li>
-            <li><Link to="/loans">Loans</Link></li>
-            <li><Link to="/investments">Investments</Link></li>
-            <li><Link to="/collections">Collections</Link></li>
+        <SideNav onSelect={(selected) => {
+            const url = '/' + selected;
+            this.props.history.push(url);
+          }}
+        >
+        <SideNav.Toggle/>
+        <SideNav.Nav defaultSelected="home">
+          <NavItem eventKey="home">
+              <NavIcon>
+                  <img src={homeImg} height="30" width="30" ></img>
+              </NavIcon>
+              <NavText>
+                Home
+                </NavText>
+          </NavItem>
+          <NavItem eventKey="clients">
+              <NavIcon>
+                  <img src={clientsImg} height="30" width="30" ></img>
+              </NavIcon>
+              <NavText>
+                Clients
+              </NavText>
+          </NavItem>
+          <NavItem eventKey="accounts">
+              <NavIcon>
+                  <img src={accImg} height="30" width="30" ></img>
+              </NavIcon>
+              <NavText>
+                Accounts
+                </NavText>
+          </NavItem>
+          <NavItem eventKey="employees">
+              <NavIcon>
+                  <img src={empImg} height="30" width="30" ></img>
+              </NavIcon>
+              <NavText>
+                Employees
+                </NavText>
+          </NavItem>
+          <NavItem eventKey="branch">
+              <NavIcon>
+                  <img src={branchImg} height="30" width="30" ></img>
+              </NavIcon>
+              <NavText>
+                Branch
+                </NavText>
+          </NavItem>
+          <NavItem eventKey="loans">
+              <NavIcon>
+                  <img src={loansImg} height="30" width="30" ></img>
+              </NavIcon>
+              <NavText>
+                Loans
+                </NavText>
+          </NavItem>
+          <NavItem eventKey="investments">
+              <NavIcon>
+                  <img src={insImg} height="30" width="30" ></img>
+              </NavIcon>
+              <NavText>
+                Investments
+                </NavText>
+          </NavItem>
+          <NavItem eventKey="collections">
+              <NavIcon>
+                  <img src={colImg} height="30" width="30" ></img>
+              </NavIcon>
+              <NavText>
+                Collections
+                </NavText>
+          </NavItem>
+  </SideNav.Nav>
 
-          </ul>
-       </nav>
-
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route path="/test" component={Test}/>
-      <Route path="/clients" component={Clients}/>
-      <Route path="/accounts" component={Accounts}/>
-      <Route path="/employees" component={Employees}/>
-      <Route path="/branch" component={Branch}/>
-      <Route path="/loans" component={Loans}/>
-      <Route path="/investments" component={Investments}/>
-      <Route path="/collections" component={Collections}/>
-    </Switch>
+  </SideNav>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/test" component={Test}/>
+        <Route path="/clients" component={Clients}/>
+        <Route path="/accounts" component={Accounts}/>
+        <Route path="/employees" component={Employees}/>
+        <Route path="/branch" component={Branch}/>
+        <Route path="/loans" component={Loans}/>
+        <Route path="/investments" component={Investments}/>
+        <Route path="/collections" component={Collections}/>
+      </Switch>
 
     </div>
     );
   }
 }
-export default App;
+export default withRouter(App);
