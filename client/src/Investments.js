4acd525a295dc0f44ddfecda3592b20a5a64181a
KaroonK <khatiwada.karoon@gmail.com>
2018-05-08 01:53:35 -0400

    Investment.js - Progress
    Completion

--- client/src/Investments.js
+++ client/src/Investments.js
@@ -6,6 +6,8 @@ import {
     AccordionItemBody,
 } from 'react-accessible-accordion';
 import axios from 'axios';
+import './styles/Investments.css';
+
 
 const JsonTable = require('ts-react-json-table');
 
@@ -15,9 +17,40 @@ class Investments extends Component {
     this.state = {
       username: '',
       isLoggedIn: false,
-      list: []
+      list: [],
+      netWorthPerson: '',
+      listNetWorth: [],
+      listPortfolioAdvisors: []
     };
     this.viewInvestments = this.viewInvestments.bind(this);
+    this.handlenetWorthChange = this.handlenetWorthChange.bind(this);
+    this.handleNetWorth = this.handleNetWorth.bind(this);
+    this.viewPortfolioAdvisors = this.viewPortfolioAdvisors.bind(this);
+  }
+
+  viewPortfolioAdvisors(){
+    var self = this;
+    axios.get('/investments/viewPortfolioAdvisors').then(function(result){
+      self.setState({listPortfolioAdvisors: result});
+    });
+  }
+  handlenetWorthChange(event){
+    this.setState({netWorthPerson: event.target.value});
+  }
+  handleNetWorth = event => {
+    event.preventDefault();
+    var self = this;
+    axios.post('/investments/checkNetWorth',{
+      netWorth: this.state.netWorthPerson
+    })
+    .then(function(response){
+      self.setState({listNetWorth : response })
+      console.log(response);
+    })
+    .catch(function (error){
+      console.log(error);
+      alert("Failed to get table.");
+    });
   }
   viewInvestments(){
     var self = this;
@@ -33,25 +66,41 @@ class Investments extends Component {
     var username = sessionStorage.getItem('username');
     return (
       <div className ="card container-fluid ">
-        Investments
         <Accordion>
           <AccordionItem onClick={this.viewInvestments}>
               <AccordionItemTitle >
-                <h4>Show Investments</h4>
+                <h4 >Show Investments</h4>
               </AccordionItemTitle>
               <AccordionItemBody>
                 <JsonTable className="table" rows={this.state.list.data}/>
               </AccordionItemBody>
           </AccordionItem>
-          <AccordionItem>
-              <AccordionItemTitle>
-                <h4>Complex title</h4>
-                <div>With a bit of description</div>
-                </AccordionItemTitle>
-                <AccordionItemBody>
-                  <p>Body content</p>
-                  </AccordionItemBody>
+          <AccordionItem >
+              <AccordionItemTitle  >
+                <div className="center"><h4>Check Portfolio Net Worth </h4></div>
+              </AccordionItemTitle>
+              <AccordionItemBody>
+                <div><form onSubmit={this.handleNetWorth}  >
+                  <label>
+                    <input type="text" value={this.state.netWorth} onChange={this.handlenetWorthChange} placeholder="Full Name"/>
+                  </label>
+                  <button type="submit"> Search</button>
+                </form>
+                  <JsonTable className="table" rows={this.state.listNetWorth.data}/>
+                </div>
+              </AccordionItemBody>
           </AccordionItem>
+          <AccordionItem onClick={this.viewPortfolioAdvisors} >
+              <AccordionItemTitle>
+                <h4>View Portfolio Advisors </h4>
+              </AccordionItemTitle>
+              <AccordionItemBody>
+                <div>
+                  <JsonTable className="table" rows={this.state.listPortfolioAdvisors.data}/>
+                </div>
+              </AccordionItemBody>
+          </AccordionItem>
+
         </Accordion>
       </div>
     );
--- routes/investments.js
+++ routes/investments.js
@@ -15,5 +15,22 @@ router.get('/viewInvestments', function(req, res, next) {
     })
   });
 
+router.get('/viewPortfolioAdvisors', function(req, res, next){
+  connection.query("SELECT Portfolio_Advisor FROM Investments WHERE Portfolio_Advisor!='Self Advised'", function (err, result, fields){
+    if(err) throw err;
+    res.send(JSON.stringify(result));
+  })
+});
+
+  router.post('/checkNetWorth', function(req, res, next) {
+    var userd = '\''+ req.body.netWorth + '\'';
+    console.log(this.userd);
+    connection.query("SELECT Name, Portfolio_num, Portfolio_worth FROM Investments p1 INNER JOIN Client p2 ON p1.Owner=p2.Ssn WHERE Name=" + userd, function (err, result,fields){
+    if (err) throw err;
+    console.log(result);
+    res.send(result);
+    })
+  });
+
 
 module.exports = router;
